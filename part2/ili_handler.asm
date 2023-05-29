de5e7005091a1153dee92f36eef97ade874d5c06
Matan Ophir <matanophir1@gmail.com>
2023-05-29 10:08:37 +0300

    before testing

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -4,4 +4,39 @@
 .align 4, 0x90
 my_ili_handler:
   ####### Some smart student's code here #######
+  pushq %rsi
+  pushq %rbx
+  pushq %rax
+
+  movq 24(%rsp), %rsi # rip_ptr
+  movzbq (%rsi), %rdi # first byte
+
+
+  movq $1, %rbx
+  cmpb %dil, $0x0F
+  jne my_ili_handler_call_what
+
+  movq $2, %rbx
+  movzbq 1(%rsi), %rdi # second byte
+
+  my_ili_handler_call_what:
+  call what_to_do
+
+  cmp %rax, $0
+  je my_ili_handler_restore_old_control
+
+  movq %rax, %rdi
+  addq %rbx, 24(%rsp)
+
+  popq %rax
+  popq %rbx
+  popq %rsi
   iretq
+
+  my_ili_handler_restore_old_control:
+
+  popq %rax
+  popq %rbx
+  popq %rsi
+  jmp* old_ili_handler
+  
--- part2/ili_utils.c
+++ part2/ili_utils.c
@@ -2,6 +2,9 @@
 
 void my_store_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
+asm("sidt %0;"
+    :"=m"(*idtr)
+        );
 
 // </STUDENT FILL>
 }
@@ -9,17 +12,30 @@ void my_store_idt(struct desc_ptr *idtr) {
 void my_load_idt(struct desc_ptr *idtr) {
 // <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
 
+asm ("lidt %0;"
+    ::"m"(*idtr)
+        );
 // <STUDENT FILL>
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+gate -> offset_high = addr >> 32;
+gate -> offset_mid = (addr << 32) >> 48;
+gate -> offset_low = (addr  << 48) >> 48;
 
 // </STUDENT FILL>
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
 // <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
+unsigned long addr = 0;
+addr = gate->offset_high;
+addr = addr << 16;
+addr = addr | gate->offset_mid;
+addr = addr << 16;
+addr = addr | gate->offset_low;
 
+return addr;
 // </STUDENT FILL>
 }
